7b3d1dd93f1f40c45c76fe94da19e4ecfd10770f
Amrik Sadhra <amriksadhra@gmail.com>
2017-02-21 21:43:48 +0000

    First commit

---
+++ src/main/java/com/LearningUnLtd/Login.java
@@ -0,0 +1,65 @@
+package com.LearningUnLtd;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import org.kordamp.bootstrapfx.scene.layout.Panel;
+
+
+public class Login extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("I^2LP");
+
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        Scene scene = new Scene(grid, 300, 275);
+        scene.getStylesheets().add("bootstrapfx.css");
+        primaryStage.setScene(scene);
+
+        Text scenetitle = new Text("Login");
+        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        grid.add(scenetitle, 0, 0, 2, 1);
+
+        Label userName = new Label("User Name:");
+        grid.add(userName, 0, 1);
+
+        TextField userTextField = new TextField();
+        grid.add(userTextField, 1, 1);
+
+        Label pw = new Label("Password:");
+        grid.add(pw, 0, 2);
+
+        PasswordField pwBox = new PasswordField();
+        grid.add(pwBox, 1, 2);
+
+        //SQL Authentication wanted here
+        Button loginButton = new Button("Login");
+        loginButton.getStyleClass().setAll("btn","btn-danger");
+        grid.add(loginButton, 1, 3, 1, 1);
+        loginButton.setOnAction(event ->{
+           MainView myMainView = new MainView();
+            myMainView.start(primaryStage);
+        });
+
+        primaryStage.show();
+    }
+}
---
+++ src/main/java/com/LearningUnLtd/MainView.java
@@ -0,0 +1,222 @@
+package com.LearningUnLtd;
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.geometry.VPos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.Stop;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import org.kordamp.bootstrapfx.scene.layout.Panel;
+
+/**
+ * Created by amriksadhra on 24/01/2017.
+ */
+public class MainView extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("I^2LP");
+
+        BorderPane border = new BorderPane();
+        Scene scene = new Scene(border, 300, 275);
+        scene.getStylesheets().add("bootstrapfx.css");
+        primaryStage.setScene(scene);
+
+        border.setTop(addHBox(primaryStage));
+        border.setLeft(addVBox());
+
+        BorderPane myPresentationElement = addPresentationElement();
+        border.setCenter(myPresentationElement);
+        border.setRight(addFlowPane());
+        border.setBottom(addStatBar(myPresentationElement));
+
+
+        primaryStage.show();
+    }
+
+    public HBox addHBox(Stage primaryStage) {
+        HBox hbox = new HBox();
+        hbox.setPadding(new Insets(15, 12, 15, 12));
+        hbox.setSpacing(10);
+        hbox.setStyle("-fx-background-color: #34495e;");
+
+        Button createPresButton = new Button("Create Presentation");
+        createPresButton.getStyleClass().setAll("btn","btn-success");
+
+        Button loadPresButton = new Button("Load Presentation");
+        loadPresButton.getStyleClass().setAll("btn", "btn-default");
+        loadPresButton.setOnAction(event ->{
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle("Open Resource File");
+            fileChooser.showOpenDialog(primaryStage);
+        });
+
+        Text platformTitle = new Text("     Integrated Interactive Learning Platform");
+        platformTitle.getStyleClass().setAll("h3");
+        platformTitle.setFill(Color.WHITESMOKE);
+
+        hbox.getChildren().addAll(createPresButton, loadPresButton, platformTitle);
+
+        return hbox;
+    }
+
+    public VBox addVBox() {
+        VBox vbox = new VBox();
+        vbox.setPadding(new Insets(10));
+        vbox.setSpacing(8);
+
+        //Generate flexible flowplane to store shape buttons
+        FlowPane shapesPane = new FlowPane();
+        shapesPane.setPadding(new Insets(5, 0, 5, 0));
+        shapesPane.setVgap(4);
+        shapesPane.setHgap(4);
+        shapesPane.setPrefWrapLength(180); // preferred width allows for two columns
+        shapesPane.setStyle("-fx-background-color: #ffffff;");
+
+        //Buttons for shapePane
+        Button triButton = new Button("Triangle");
+        triButton.getStyleClass().setAll("btn","btn-success");
+        shapesPane.getChildren().add(triButton);
+        Button squareButton = new Button("Square");
+        squareButton.getStyleClass().setAll("btn","btn-warning");
+        shapesPane.getChildren().add(squareButton);
+        Button circButton = new Button("Circle");
+        circButton.getStyleClass().setAll("btn","btn-info");
+        shapesPane.getChildren().add(circButton);
+        Button starButton = new Button("Star");
+        starButton.getStyleClass().setAll("btn","btn-danger");
+        shapesPane.getChildren().add(starButton);
+
+        //Create Panel for shapes
+        Panel shapes = new Panel();
+        shapes.setText("Shapes");
+        shapes.getStyleClass().add("panel-primary");
+        shapes.setBody(shapesPane);
+        VBox.setMargin(shapes, new Insets(0, 0, 0, 0));
+        vbox.getChildren().add(shapes);
+
+        //Generate flexible flowplane to store text buttons
+        FlowPane textPane = new FlowPane();
+        textPane.setPadding(new Insets(5, 0, 5, 0));
+        textPane.setVgap(4);
+        textPane.setHgap(4);
+        textPane.setPrefWrapLength(180); // preferred width allows for two columns
+        textPane.setStyle("-fx-background-color: #ffffff;");
+
+        //Buttons for textPane
+        Button boldButton = new Button("Bold");
+        boldButton.getStyleClass().setAll("btn","btn-default");
+        textPane.getChildren().add(boldButton);
+        Button italButton = new Button("Itallic");
+        italButton.getStyleClass().setAll("btn","btn-default");
+        textPane.getChildren().add(italButton);
+        Button underButton = new Button("Underline");
+        underButton.getStyleClass().setAll("btn","btn-default");
+        textPane.getChildren().add(underButton);
+
+        //Create panel for text controls
+        Panel text = new Panel();
+        text.setText("Text");
+        text.getStyleClass().add("panel-default");
+        text.setBody(textPane);
+        VBox.setMargin(text, new Insets(0, 0, 0, 0));
+        vbox.getChildren().add(text);
+
+        return vbox;
+    }
+
+    public BorderPane addPresentationElement() {
+        BorderPane border = new BorderPane();
+
+        // Category in column 2, row 1
+        Text category = new Text("PRESENTATION HERE");
+        category.setFont(Font.font("San Francisco", FontWeight.NORMAL, 20));
+        border.setCenter(category);
+
+        return border;
+    }
+
+    public ScrollPane addFlowPane() {
+        ScrollPane scroll = new ScrollPane();
+        scroll.setFitToWidth(true);
+
+        FlowPane flow = new FlowPane();
+        flow.setPadding(new Insets(5, 0, 5, 0));
+        flow.setVgap(4);
+        flow.setHgap(4);
+        flow.setPrefWrapLength(170); // preferred width allows for two columns
+        flow.setStyle("-fx-background-color: #ffffff;");
+
+        int numSlides = 20;
+
+        Panel[] slides = new Panel[numSlides];
+
+        for(int i = 0; i < numSlides; i ++){
+            slides[i] = new Panel();
+            slides[i].getStyleClass().add("panel-primary");
+            slides[i].setBody(new Text("Slide panel preview here."));
+            slides[i].setPrefWidth(170);//Dynamic resizing of panel width possible?
+            flow.getChildren().add(slides[i]);
+            flow.setMargin(slides[i], new Insets(0, 20, 0, 5));
+        }
+
+        scroll.setContent(flow);
+
+        return scroll;
+    }
+
+    public HBox addStatBar(BorderPane presentationElement) {
+        HBox hbox = new HBox();
+        hbox.setPadding(new Insets(0, 12, 0, 12));
+        hbox.setSpacing(2);
+        hbox.setStyle("-fx-background-color: #34495e;");
+
+        BorderPane border = new BorderPane();
+
+        Text versionText = new Text("Version 0.0.1 Alpha");
+        versionText.setFont(Font.font("San Francisco", FontWeight.NORMAL, 12));
+        versionText.setFill(Color.WHITE);
+
+        border.setLeft(versionText);
+
+        border.setCenter(new Text("                                                     "));
+
+        Text coordTextBar = new Text("Mouse data not available!");
+        coordTextBar.setFont(Font.font("San Francisco", FontWeight.NORMAL, 12));
+        coordTextBar.setFill(Color.WHITE);
+
+        border.setRight(coordTextBar);
+
+       presentationElement.setOnMouseMoved(event -> coordTextBar.setText(
+                "(x: "       + event.getX()      + ", y: "       + event.getY()       + ") -- " +
+                        "(sceneX: "  + event.getSceneX() + ", sceneY: "  + event.getSceneY()  + ") -- " +
+                        "(screenX: " + event.getScreenX()+ ", screenY: " + event.getScreenY() + ")"));
+
+        presentationElement.setOnMouseExited(event -> coordTextBar.setText("Mouse Exited"));
+
+        hbox.getChildren().addAll(border);
+
+        return hbox;
+    }
+}
+
