1b686fd4d14e779fab3ae46c1034045c30d02122
Herman Brunbäck Larsen <herrlarsen@gmail.com>
2017-02-24 14:02:32 +0000

    Added a psudo parser, non functional. Added basic classes to expand on for presentation, slide and slideElement which can be used to inherit to all other elements.
    Signed-off-by: Herman Brunbäck Larsen <herrlarsen@gmail.com>

--- src/main/java/utilities/Presentation.java
+++ src/main/java/utilities/Presentation.java
@@ -1,7 +1,33 @@
 package utilities;
 
+import java.util.List;
+import java.util.ArrayList;
+
 /**
  * Created by habl on 23/02/2017.
  */
 public class Presentation {
+    private String documentID;
+    private String author;
+    private Float version;
+    private Float documenAspectRatio;
+    private String tags;
+    private int groupFormat;
+
+
+    private List<Slide> slideList;
+
+    public Presentation () {
+        slideList = new ArrayList<Slide>();
+    }
+
+    public void addSlide() {
+
+    }
+
+    public void deleteSlide() {
+
+    }
+
+
 }
