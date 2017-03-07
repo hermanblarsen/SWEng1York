cf3bd61252b05402bc11e53a83b38b45607ff861
Herman Brunbäck Larsen <herrlarsen@gmail.com>
2017-03-07 10:22:10 +0000

    Major refactoring from interface to abstract SlideElement class, to be merged with Amriks and Koens work
    Signed-off-by: Herman Brunbäck Larsen <herrlarsen@gmail.com>

--- src/main/java/utilities/SlideElement.java
+++ src/main/java/utilities/SlideElement.java
@@ -6,15 +6,34 @@ import javafx.scene.layout.Pane;
 /**
  * Created by habl on 23/02/2017.
  */
-public interface SlideElement  {
-    //Empty interface for tagging our actual slide elements
-    //TODO whoever put this here, the interface is supposed to be fully empty, or it might cause problems with externally produced Objects
-    //TODO find a way to implement this properly, maybe by casting or something somewhere else?
-    void renderElement(int animationType);
-    Node getCoreNode();
-    void setSlideCanvas(Pane slideCanvas);
-    int getLayer();
-    int getStartSequence();
-    int getEndSequence();
-    void setVisibility(boolean visibility);
+public abstract class SlideElement  {
+
+    public void renderElement(int animationType) {
+
+    }
+
+    public abstract Node getCoreNode();
+
+    public void setSlideCanvas(Pane slideCanvas) {
+
+    }
+
+    public int getLayer(){
+
+        return 0;
+    }
+
+    public int getStartSequence(){
+
+        return 0;
+    }
+
+    public int getEndSequence() {
+
+        return 0;
+    }
+
+    public void setVisibility(boolean visibility) {
+
+    }
 }
