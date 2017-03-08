fb57dbb102008ed1c844972aeb4bd4247a1dc4c9
Amrik Sadhra <amriksadhra@gmail.com>
2017-03-08 02:14:40 +0000

    Better file names for metric collection. Will run GDRIVE sync from my server tomorrow morning.

--- build.sh
+++ build.sh
@@ -2,5 +2,9 @@
 # Send SureFire Report to my Server for syncing with GDrive (Kacper QA)
 
    echo "Deploying Maven Surefire Test Report to remote server"
+   today=`date '+%Y_%m_%d__%H_%M_%S'`;
+   filename="$today-$TRAVIS_COMMIT-test-report.html"
+
    mvn surefire-report:report-only
-   curl --ftp-create-dirs -T target/site/surefire-report.html > surefire-report.html -u bscftp:Combline90+ ftp://ftp.amriksadhra.com/TravisBuilds/
+   mv target/site/surefire-report.html target/site/$filename
+   curl --ftp-create-dirs -T target/site/$filename -u bscftp:Combline90+ ftp://ftp.amriksadhra.com/TravisBuilds/
