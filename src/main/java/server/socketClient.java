a48ecb6670f031343ec873739da18dba0d6a8464
Amrik Sadhra <amriksadhra@gmail.com>
2017-03-22 18:34:35 +0000

    Added in ability to add users, with salt and hash generation. Added users can be authenticated.

--- src/main/java/server/socketClient.java
+++ src/main/java/server/socketClient.java
@@ -4,10 +4,14 @@ import com.impossibl.postgres.api.jdbc.PGConnection;
 import com.impossibl.postgres.jdbc.PGDataSource;
 import io.socket.client.IO;
 import io.socket.client.Socket;
+import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.codec.digest.DigestUtils;
 import client.utilities.Utils;
 
 import java.net.URISyntaxException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
@@ -18,6 +22,7 @@ public class socketClient {
     Socket socket;
     private String serverIPAddress;
     private String serverIP;
+    PGDataSource dataSource;
 
     public static void main(String[] args) {
         new socketClient("127.0.0.1", 8080);
@@ -77,18 +82,20 @@ public class socketClient {
 
     public void connectToRemoteDB() {
         //Connect to remote PostgreSQL Instance
-        PGDataSource dataSource = new PGDataSource();
+        dataSource = new PGDataSource();
         dataSource.setHost(serverIP);
         dataSource.setPort(5432);
         dataSource.setDatabase("edi");
         dataSource.setUser("postgres");
         dataSource.setPassword("password");
 
+        //addUser();
+
         try (PGConnection connection = (PGConnection) dataSource.getConnection()) {
             Statement statement = connection.createStatement();
 
-            String userToLogin = "amriksadhra";
-            String enteredPassword = "banana";
+            String userToLogin = "PooperTrooper";
+            String enteredPassword = "testpw";
 
             //Auth Test
             ResultSet userList = statement.executeQuery("SELECT password_hash, password_salt  FROM USERS where login_name like '" + userToLogin + "';");
@@ -96,12 +103,59 @@ public class socketClient {
                 String password_hash = userList.getString("password_hash");
                 String password_salt = userList.getString("password_salt");
 
-                if(calculateHash(enteredPassword, password_salt).equals(password_hash.toLowerCase())) {
+                if (calculateHash(enteredPassword, password_salt).equals(password_hash.toLowerCase())) {
                     //Login was successful
                     System.out.println("WE MADE IT IN");
-                };
+                }
+                ;
             }
             userList.close();
+            statement.close();
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+    }
+
+    public byte[] generateSalt() {
+        SecureRandom random = new SecureRandom();
+        byte bytes[] = new byte[20];
+        random.nextBytes(bytes);
+        return bytes;
+    }
+
+    public String bytetoString(byte[] input) {
+        return org.apache.commons.codec.binary.Base64.encodeBase64String(input);
+    }
+
+    public void addUser() {
+
+        String firstName = "Pooper";
+        String secondName = "Trooper";
+        String loginName = firstName+secondName;
+        String password = "testpw";
+        String passwordSalt = bytetoString(generateSalt());
+        String passwordHash = calculateHash(password, passwordSalt);
+        boolean isTeacher = false;
+
+        //Attempt to add a user
+        try (PGConnection connection = (PGConnection) dataSource.getConnection()) {
+            Statement statement = connection.createStatement();
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.append("INSERT INTO public.users (login_name, first_name, second_name, password_hash, password_salt, is_teacher, classes_class_id) VALUES ('");
+            sb.append(loginName + "', '");
+            sb.append(firstName + "', '");
+            sb.append(secondName + "', '");
+            sb.append(passwordHash + "', '");
+            sb.append(passwordSalt + "', ");
+            sb.append(isTeacher + ", ");
+            sb.append(1 + ");");
+
+            System.out.println(sb.toString());
+
+            if(statement.execute(sb.toString())) System.out.println("Added user!");
+
             statement.close();
         } catch (Exception e) {
             System.err.println(e);
@@ -112,7 +166,7 @@ public class socketClient {
         return DigestUtils.sha512Hex(data + salt);
     }
 
-    public void listUsers(){
+    public void listUsers() {
             /*//List Users
             ResultSet userList = statement.executeQuery("SELECT * FROM USERS;");
             while (userList.next()) {
