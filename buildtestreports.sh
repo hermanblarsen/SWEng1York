f38ee48d594ae2aa17b81ec8c89ed508d40720f5
LukeDummott <luke.dummott@gmail.com>
2017-04-14 15:45:04 +0100

    Update buildtestreports.sh

--- buildtestreports.sh
+++ buildtestreports.sh
@@ -3,7 +3,7 @@
 
    echo "Deploying Maven Surefire Test Report to remote server"
    today=`date '+%Y_%m_%d__%H_%M_%S'`;
-   filename="$today-$TRAVIS_COMMIT-test-report.html"
+   filename="$today-$CIRCLE_SHA1-test-report.html"
 
    mvn surefire-report:report-only
    mv target/site/surefire-report.html target/site/$filename
