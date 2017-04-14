3617b414e95daf7457b8028303400a9960c3cc19
Amrik Sadhra <amriksadhra@gmail.com>
2017-04-14 18:09:04 +0100

    Make the circle CI build create nice artifacts. Get Gdrive working (and build passing).

--- buildtestreports.sh
+++ buildtestreports.sh
@@ -5,10 +5,18 @@ echo "Generating Maven Surefire test report"
 today=`date '+%Y_%m_%d__%H_%M_%S'`;
 filename="$today-$CIRCLE_SHA1-test-report.html"
 
+# Generate reports and rename to contain date, time, commit SHA1
 mvn surefire-report:report-only
 mv target/site/surefire-report.html target/site/$filename
 
+# Make gdrive executable
+chmod a+x /bin/gdrive
 
 echo "Uploading HTML report to google drive"
  ./bin/gdrive --refresh-token "$GDRIVE_REFRESH_TOKEN" upload --parent "$GDRIVE_DIR" target/site/$filename
 
+# Make Artifact collection easy on Circle by moving to some Dirs
+mkdir -p "Test Reports/junit/"
+find . -type f -regex ".*/target/surefire-reports/.*xml" -exec cp {} "Test Reports/junit/" \;
+find . -type f -regex ".*/target/site/.*html" -exec cp {} "Test Reports/junit/" \;
+find . -type f -regex ".*/target/.*jar" -exec cp {} "JAR Files/" \;
