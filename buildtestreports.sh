62ffb2a05cf0492affe925c2fb99ed84d4efa906
Amrik Sadhra <amriksadhra@gmail.com>
2017-04-14 17:46:12 +0100

    Add GDrive environment variables for HTML reports. Alter POM to produce two jar files; client and server (removed default jar build).

--- buildtestreports.sh
+++ buildtestreports.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-# Send SureFire Report to my(Amrik's) Server for syncing with GDrive (Kacper QA)
+# Send SureFire Report to Google Drive for syncing with GDrive (Kacper QA)
 
-   echo "Deploying Maven Surefire Test Report to remote server"
-   today=`date '+%Y_%m_%d__%H_%M_%S'`;
-   filename="$today-$CIRCLE_SHA1-test-report.html"
+echo "Generating Maven Surefire test report"
+today=`date '+%Y_%m_%d__%H_%M_%S'`;
+filename="$today-$CIRCLE_SHA1-test-report.html"
 
-   mvn surefire-report:report-only
-   mv target/site/surefire-report.html target/site/$filename
+mvn surefire-report:report-only
+mv target/site/surefire-report.html target/site/$filename
+
+
+echo "Uploading HTML report to google drive"
+ ./bin/gdrive --refresh-token "$GDRIVE_REFRESH_TOKEN" upload --parent "$GDRIVE_DIR" target/site/$filename
+
