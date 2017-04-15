3acbe3b17091f1883cab26bb41f035a2a95eb35c
Amrik Sadhra <amriksadhra@gmail.com>
2017-04-15 15:56:50 +0100

    Fix for Droplet deployment

--- post-build.sh
+++ post-build.sh
@@ -30,11 +30,11 @@ echo "Updating Droplet SocketServer instance with fresh server jar"
 
 # Connect to Droplet so can run fresh SocketServer
 echo "-- Connecting via SSH to kill current SocketServer instance"
-ssh -i  bscftp@ssh.amriksadhra.com 'killall screen | rm -rf /home/bscftp/CircleBuilds/ | exit'
+ssh bscftp@ssh.amriksadhra.com 'killall screen | rm -rf /home/bscftp/CircleBuilds/ | exit'
 
 # Upload Jar file to Droplet instance
 curl --ftp-create-dirs -T target/*server-jar-with-dependencies.jar -u bscftp:Combline90+ ftp://ftp.amriksadhra.com/CircleBuilds/
 
 echo "-- Connecting via SSH to start new SocketServer instance"
-ssh -i  bscftp@ssh.amriksadhra.com 'screen -m -d java -jar /home/bscftp/CircleBuilds/*.jar'
+ssh bscftp@ssh.amriksadhra.com 'screen -m -d java -jar /home/bscftp/CircleBuilds/*.jar'
 
