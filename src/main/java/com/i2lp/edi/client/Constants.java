cfd16b4d4d04a18618fab052b81c323efc0ceb15
Amrik Sadhra <amriksadhra@gmail.com>
2017-04-22 17:39:39 +0100

    (Actual) Addition of TimePicker for Kacper

--- src/main/java/com/i2lp/edi/client/Constants.java
+++ src/main/java/com/i2lp/edi/client/Constants.java
@@ -8,6 +8,6 @@ public class Constants {
      // see https://docs.oracle.com/middleware/1212/core/MAVEN/maven_version.htm#MAVEN8903
 
     //Build number and string for use in About dialogues.
-    private static final String BUILD_NUMBER = "27";
+    private static final String BUILD_NUMBER = "28";
     public static final String BUILD_STRING = "Version: v0." + Constants.BUILD_NUMBER;
 }
